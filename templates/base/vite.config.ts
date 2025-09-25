41b85d104eda47c04428a52e90e13e20626a1889
Vilmos Hosztafi <hosztafi@vilmos.co.uk>
2025-09-25 22:03:36 +0100

    chore: add configuration and tooling for linting, formatting, testing, and building

--- templates/base/vite.config.ts
+++ templates/base/vite.config.ts
@@ -1,18 +1,18 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./tests/setup.ts'],
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
   },
 })
